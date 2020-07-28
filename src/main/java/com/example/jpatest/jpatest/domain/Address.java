b32b648f7e77bfb79a004a846e5c2e52f8426874
JUNH4 <s2juneha@gmail.com>
2020-07-28 21:48:58 +0900

    Initial commit

---
+++ src/main/java/com/example/jpatest/jpatest/domain/Address.java
@@ -0,0 +1,14 @@
+package com.example.jpatest.jpatest.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter @Setter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
---
+++ src/main/java/com/example/jpatest/jpatest/domain/DeliveryStatus.java
@@ -0,0 +1,5 @@
+package com.example.jpatest.jpatest.domain;
+
+public enum DeliveryStatus {
+    READY, COMP
+}
